140996ed357b327c6b2ae11bbba997144e8a4208
Joy Yeh <66350727+JoyYeh2002@users.noreply.github.com>
2023-07-05 10:33:06 -0400

    Add files via upload

---
+++ Sharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using OriginalCircuit.AltiumSharp;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string fileName = "C:\\Users\\joy20\\Folder\\NASA\\07-04_Altium_Sharp\\LQMS_BOB_78.PcbLib";
+
+// Open a PcbLib file.
+	using (var reader = new PcbLibReader())
+	{
+	    // Read the file.
+	    OriginalCircuit.AltiumSharp.PcbLib data = reader.Read(fileName);
+
+	    // Iterate through each component in the library.
+	    foreach (var component in data.Items)
+	    {
+	        // Print information about the component.
+	        Console.WriteLine($"Name: {component.Pattern}");
+            Console.WriteLine($"Description: {component.Description}");
+	        Console.WriteLine($"Number of Pads: {component.Pads}");
+	        Console.WriteLine($"Number of Primitives: {component.Primitives.Count()}\n");
+	    }
+
+	    // Retrieve settings from the header.
+	    OriginalCircuit.AltiumSharp.BasicTypes.Unit _displayUnit = data.Header.DisplayUnit;
+	    OriginalCircuit.AltiumSharp.BasicTypes.Coord _coord = data.Header.SnapGridSize;
+
+        Console.WriteLine($"Display Unit: {_displayUnit}");
+        Console.WriteLine($"Coord: {_coord}");
+
+	    // OriginalCircuit.AltiumSharp.BasicTypes. = data.Items.Cast<IComponent>().ToList();
+	}
