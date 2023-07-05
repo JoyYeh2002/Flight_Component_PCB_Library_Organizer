4a844f20d3c17176d4254e540873df2f5615ba3b
Joy Yeh <66350727+JoyYeh2002@users.noreply.github.com>
2023-07-05 13:10:43 -0400

    Made test_lib.schlib and updated path

--- Sharp/Program.cs
+++ Sharp/Program.cs
@@ -1,9 +1,10 @@
-﻿using OriginalCircuit.AltiumSharp;
+using OriginalCircuit.AltiumSharp;
 
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-string fileName = "C:\\Users\\joy20\\Folder\\NASA\\07-04_Altium_Sharp\\LQMS_BOB_78.PcbLib";
+//string fileName = "C:\\Users\\joy20\\Folder\\NASA\\07-04_Altium_Sharp\\LQMS_BOB_78.PcbLib";
+string fileName = "C:\\Users\\joy20\\Folder\\NASA\\07-04_Altium_Sharp\\Temp_Project_Lib_Test\\test_lib.PcbLib";
 
 // Open a PcbLib file.
 	using (var reader = new PcbLibReader())
@@ -15,10 +16,10 @@ string fileName = "C:\\Users\\joy20\\Folder\\NASA\\07-04_Altium_Sharp\\LQMS_BOB_
 	    foreach (var component in data.Items)
 	    {
 	        // Print information about the component.
-	        Console.WriteLine($"Name: {component.Pattern}");
+	        Console.WriteLine($"Name: {component.Pattern}\n");
             Console.WriteLine($"Description: {component.Description}");
 	        Console.WriteLine($"Number of Pads: {component.Pads}");
-	        Console.WriteLine($"Number of Primitives: {component.Primitives.Count()}\n");
+	        Console.WriteLine($"Number of Primitives: {component.Primitives.Count()}");
 	    }
 
 	    // Retrieve settings from the header.
